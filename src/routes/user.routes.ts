ee8976dd06a1dedcb4c7c241a4ca30b8fa17d54c
Samy Senani <samy.senani@deepor.ai>
2021-12-22 14:54:58 +0100

    add: user schema

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,14 @@
 import { Express, Request, Response } from "express";
 
+import { createUserHandler } from "../controllers/user.controller";
+import { validateRequest } from "../middlewares";
+import { createUserSchema } from "../schema/user.schema";
 
 export default function (app: Express) {
-  app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+//   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
-//   // Register user
-//   app.post("/api/users", validateRequest(createUserSchema), createUserHandler);
+  // Register user
+  app.post("/api/users", validateRequest(createUserSchema), createUserHandler);
 
 //   // Login
 //   app.post(
