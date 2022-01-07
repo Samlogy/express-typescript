d8972c946abef792a7215f2be5932ed2763d361f
Samy Senani <samy.senani@deepor.ai>
2022-01-07 19:35:03 +0100

    add: generate key function

---
+++ src/utils/generateKey.ts
@@ -0,0 +1,15 @@
+
+export const generateKey = (length: number): string => {
+    let result = '';
+
+    const lower = 'abcdefghijklmnopkrstvwxyz'
+    const upper = 'ABCDEFGHIJKLMNOPKRSTVWXYZ';
+    const numbers = "0123456789";
+    const specials = ".,;:!?-_=+*°&#";
+    const all = lower + upper + numbers + specials;
+    
+    for ( let i = 0; i < length; i++ ) {
+        result += all.charAt(Math.floor(Math.random() * all.length));
+    }
+    return result;
+};
