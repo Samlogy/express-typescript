f512b1c7efed7812ee29cb30c449afd2e47dd179
Samy Senani <samy.senani@deepor.ai>
2022-01-08 11:32:06 +0100

    add generate code function

--- src/utils/generateKey.ts
+++ src/utils/generateKey.ts
@@ -13,3 +13,17 @@ export const generateKey = (length: number): string => {
     }
     return result;
 };
+
+export const generateCode = (length: number): string => {
+    let result = '';
+
+    const lower = 'abcdefghijklmnopkrstvwxyz'
+    const upper = 'ABCDEFGHIJKLMNOPKRSTVWXYZ';
+    const numbers = "0123456789";
+    const all = lower + upper + numbers;
+    
+    for ( let i = 0; i < length; i++ ) {
+        result += all.charAt(Math.floor(Math.random() * all.length));
+    }
+    return result;
+};
